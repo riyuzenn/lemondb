cfa082b80c40d2006d6d74322e6ec2d454171a3b
Zenqi <66673988+znqi@users.noreply.github.com>
2021-09-08 18:14:06 +0800

    :tada: Create `middleware`

---
+++ lemon/middleware/json.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2021 Zenqi
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import pathlib
+from lemondb.middleware.base import BaseMiddleware
+from lemondb.serializer import JSONSerializer
+from lemondb.types import (
+    Optional,
+    Mapping,
+    Dict,
+    Any
+)
+from lemondb.utils import iterate_dict
+
+class JsonMiddleware(BaseMiddleware):
+    """
+    A main class for handling JSON data inorder to read
+    and write and delete item.
+    """
+    
+    serializer: JSONSerializer
+
+    def __init__(self, serializer: Optional[str] = 'json'):
+        self.serializer = JSONSerializer(serializer)
+        super().__init__()
+        
+    def read(self, path: str) -> Dict[str, Any]:
+        """
+        Read the file and return the data
+
+        Parameter:
+            path (str):
+                The path for the file to be red. It can 
+                be pathlib.Path instance or a str.
+        """
+
+        if isinstance(path, pathlib.Path):
+            path = path.absolute()
+        else:
+            path = path
+
+        with open(path, 'r+') as f:
+            return self.serializer.load(f)
+
+    def write(self, item: Mapping, path: str):
+        """
+        Write the given data to the file
+        """
+        if isinstance(path, pathlib.Path):
+            path = path.absolute()
+        else:
+            path = path
+
+        if not path.exists():
+            raise FileNotFoundError('"%s" not found' % (path.absolute()))
+
+        data = self.read(path); data.update(item)
+        with open(path, 'r+') as f:
+            self.serializer.dump(data, f)
+
+            
+    def delete(self, key: str, path: str):
+        """
+        Delete the given key and write to the given
+        file path
+        """
+
+        raw = []
+        data = self.read(path)
+        for table,v in data.items():
+            for k,v in v.items():
+                raw.append(v)
+                if key == v:
+                    del data[table][k]
+                    break
+
+        
+        
+        self.write(data, path)
---
+++ lemon/middleware/sidle.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2021 Zenqi
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+try:
+    from sidle.enc import SidleEncryption
+    import sidle.errors as error
+except ModuleNotFoundError:
+    SidleEncryption = None
+
+
+from lemondb.middleware import BaseMiddleware
+from lemondb.types import (
+    Optional,
+    Mapping,
+    Dict,
+    Any
+)
+import pathlib
+from lemondb.serializer import JSONSerializer
+
+class SidleMiddleware(BaseMiddleware):
+    """
+    A encryption middleware for `SidleEncryption`.
+    That can encrypted with password
+    """
+
+    serializer: SidleEncryption
+    def __init__(self, password: str, jsonserializer: Optional[str] = 'json'):
+        """
+        Initialize SidleMiddleware.
+
+        Parameter:
+            password (str):
+                The password key for the database.
+        """
+
+        if not SidleEncryption:
+            raise ModuleNotFoundError('Sidle is not installed. `pip install sidle` might solved this')
+
+        self.password = password
+        self.serializer = SidleEncryption(self.password)
+        self.json = JSONSerializer(jsonserializer)
+
+    def read(self, path: str) -> Dict[str, Any]:
+        if isinstance(path, pathlib.Path):
+            path = path.absolute()
+        else:
+            path = path
+
+
+        with open(path, 'rb') as f:
+            raw = self.serializer.decrypt(
+                f.read()
+            )
+            if not raw:
+                raise error.PasswordError("\"%s\" is not a valid password" % (self.password))
+
+            data = self.json.loads(
+                s=raw
+            )
+        
+        return data
+
+    def write(self, item: Mapping, path: str):
+        """
+        Write the given item with encryption
+        """
+        if isinstance(item, dict):
+            item = self.json.dumps(item)
+        else:
+            item = item
+
+        item = self.serializer.encrypt(item)
+
+        if isinstance(path, pathlib.Path):
+            path = path.absolute()
+        else:
+            path = path
+            
+        with open(path, 'wb') as f:
+            f.write(item)
