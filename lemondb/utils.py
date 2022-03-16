d1d2eeeda3a1d2f5776dae17c5f92256d04b2d92
Zenqi <66673988+zxnqi@users.noreply.github.com>
2022-03-16 16:36:17 +0800

    :sparkles: Added `typenize`

--- lemondb/utils.py
+++ lemondb/utils.py
@@ -19,7 +19,79 @@
 # SOFTWARE.
 
 
-from typing import Mapping
+from lemondb.types import (
+    Mapping,
+    datetime
+)
+type_mapping = {
+    str: 'string',
+    bytes: 'bytes',
+    list: 'list',
+    tuple: 'tuple',
+    int: 'int',
+    datetime: 'datetime'
+}
+
+
+def _typenize(data: dict):
+    l = []
+    _l = []
+    for k,v in data.items():
+        if isinstance(v, dict):
+            _d, _l = _typenize(v)
+        
+        _type = type_mapping.get(type(v), None)
+        
+        if _type:
+            l.append(_type)
+        elif _l:
+            l.append(_l)
+
+        if isinstance(v, bytes):
+            v = v.decode()
+        elif isinstance(v, datetime):
+            v = v.isoformat()
+
+        data.update({k:v})
+        
+    return data, l
+
+def typenize(data: dict):
+    d, t = _typenize(data)
+    if t:
+        data.update({'$type': t})
+    else:
+        data.update({'$type': []})    
+    return data
+
+def untypenize(data):
+    t = dict((v,k) for k,v in type_mapping.items())
+    
+    types = data.pop('$type')
+    
+    for d,i in zip(data.items(), types):
+        k,v = d
+        if isinstance(i, list) and isinstance(v, dict):
+            v.update({'$type': i})
+            untypenize(v)
+
+        else:
+            _type = t[i]
+
+
+            if isinstance(_type, type):
+                if _type == bytes:
+                    v = v.encode()
+                elif _type == datetime:
+                    v = datetime.fromisoformat(v)
+                else:
+                    v =  _type(v)
+
+            data.update({k:v})
+
+    return data
+
+
 
 def iterate_dict(item: Mapping):
     """
